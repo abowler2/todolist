11a858747a122befd86dbe3cc666e0f917d65986
abowler <abowler2@gmail.com>
2019-09-28 14:08:03 -0700

    added ability to add a todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Todos from "./components/Todos";
 import Header from "./components/Header";
+import AddTodo from "./components/AddTodo";
 import './App.css';
 
 
@@ -42,13 +43,28 @@ class App extends Component {
     })
   }
 
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
   render() {
     return (
       <div className = "App" >
+        <div className="container">
           <Header />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete=
           { this.markComplete }
           delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
